5b387fe626a92dcb84b63e6b168571b9dcf7da16
Tom Donohue <foxun@hotmail.com>
2016-03-11 20:14:36 +0000

    Initial commit

---
+++ footer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Stepney
+ */
+
+$my_theme = wp_get_theme();
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'stepney' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'stepney' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'stepney' ), '<a href="'.$my_theme->get('ThemeURI').'">Stepney</a>', '<a href="http://monodot.co.uk" rel="designer">Tom Donohue</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
